a81389a4cb62b2581fc0d900ed23de33bb07c6f2
Anibal Amoroso <anibaldaniel2212@gmail.com>
2021-11-08 16:24:04 -0300

    Add content to index.html style.css index.js and interactive.js

---
+++ index.js
@@ -0,0 +1,33 @@
+import './style.css';
+import Todos from './interactive.js';
+
+const div = document.getElementById('add');
+
+const input = document.createElement('input');
+input.type = 'text';
+input.placeholder = 'Add to do...';
+input.id = 'text';
+input.setAttribute('require',true)
+const addButton = document.createElement('button');
+
+addButton.innerText = 'Add';
+addButton.type = 'submit';
+div.append(input, addButton);
+
+const listTodo = new Todos();
+
+div.addEventListener('submit', (e) => {
+  e.preventDefault();
+  listTodo.allTodos.push({
+    description: input.value,
+    complete: false,
+    id: listTodo.allTodos.length + 1,
+  });
+
+  listTodo.saveLocal();
+
+  listTodo.createHtml();
+  input.value = '';
+  input.focus();
+});
+listTodo.createHtml(
